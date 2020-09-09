ffc4693d589c7c43d546568fa1e328af01134982
Andrew Matthews <matthews.andrew@gmail.com>
2020-09-10 09:31:58 +1000

    docs

--- src/fifthlang/FuncWrapper.cs
+++ src/fifthlang/FuncWrapper.cs
@@ -4,6 +4,16 @@ using System.Text;
 
 namespace fifth
 {
+    /// <summary>
+    /// A wrapper around any sort of function, to make it easier to extract and perform type
+    /// checking on its type parameters and result type.
+    /// </summary>
+    /// <remarks>
+    /// This becomes important when type checking is performed on functions that have been pushed
+    /// onto the stack. The interpreter needs to know whether the types of the items below the
+    /// function on the stack are compatible with the function. It could make use of reflection, but
+    /// this is much simpler, and probably faster.
+    /// </remarks>
     public class FuncWrapper
     {
         public List<Type> ArgTypes { get; }
